f107a5215bd5b7feac5d0203f8096fc3d6207892
iliaamb <iliablohari@gmail.com>
2024-06-08 10:27:09 +0330

    add start menu method

--- rakab new  edition.cpp
+++ rakab new  edition.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 class Menu{///for setting the menu of game
 public:
+void Start_menu(){
+        system("cls");
+        cout << " Welcome to rakab Game!!"<<endl << endl<< "\t ***************" << endl<<endl;
+        cout << "\t 1-Start the game" << endl << "\t 2-Menu of opsions" << endl << "\t 3-Exit" << endl<<endl;
+        cout << "\t ***************"<<endl;
+    }
 private:
 };
