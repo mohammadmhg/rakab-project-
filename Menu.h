b03db53744ccedc5e42fa6c452fc632215916e44
mohammadmhg <mohammadmhg83@gmail.com>
2024-06-28 20:53:08 +0330

    create the header of Menus class

---
+++ Menu.h
@@ -0,0 +1,24 @@
+
+#ifndef MENU_H
+#define MENU_H
+
+
+class Menus{
+public:
+    void start_menu();
+    void start_war(int);
+    void menu_of_opsions();
+    void bahar_help();
+    void zemestan_help();
+    void tabl_zan_help();
+    void shah_dokht_help();
+    void matarsak_help();
+    void non_existed_card();
+    bool action();
+    void exit();
+    void ending();
+    void enter_players();
+    void enter_details();
+};
+
+#endif
