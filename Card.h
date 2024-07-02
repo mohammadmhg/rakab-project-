c173e1528ea439fdcf1f2ae43cc0adca66ba27a4
mohammadmhg <mohammadmhg83@gmail.com>
2024-07-02 19:31:15 +0330

    create the header of card class(not finished)

---
+++ Card.h
@@ -0,0 +1,31 @@
+#include <vector>
+#ifndef CARD_H
+#define CARD_H
+using namespace std;
+
+class Card{
+public:
+    Card();
+    Card(string,int);
+    ///setter
+
+    void set_yellow_card();
+    void set_purple_card();
+    ///getter
+
+    int get_yellow_card(int) const;
+    string get_purple_card(int) const;
+    ///method
+
+    void shuffle_cards();
+
+private:
+    vector <string> purple_cards;
+    vector <int> yellow_cards;
+    void define_purple_cards(int,string);
+    void define_yellow_cards(int,int);
+
+};
+
+
+#endif // CARD_H
