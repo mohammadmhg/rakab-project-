4dd5069b2739ce32d7ff673c206f9f8f47eff836
mohammadmhg <mohammadmhg83@gmail.com>
2024-07-02 19:53:12 +0330

    added some function, corrected the code

--- Card.cpp
+++ Card.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <ctime>
 #include "Card.h"
 
 using namespace std;
@@ -20,12 +21,28 @@ using namespace std;
     void Card::set_yellow_card()
     {
         yellow_cards.resize(58);
+        Card::define_yellow_cards(10,1);//set soldier with 1 power
+        Card::define_yellow_cards(8,2);//set soldier with 2 power
+        Card::define_yellow_cards(8,3);//set soldier with 3 power
+        Card::define_yellow_cards(8,4);//set soldier with 4 power
+        Card::define_yellow_cards(8,5);//set soldier with 5 power
+        Card::define_yellow_cards(8,6);//set soldier with 6 power
+        Card::define_yellow_cards(8,10);//set soldier with 10 power
+        Card::shuffle_yellow_cards();
+
 
     }
     void Card::set_purple_card()
     {
         purple_cards.resize(52);
         Card::define_purple_cards(16,"matarsak");//set matarsak
+        Card::define_purple_cards(3,"bahar");//set bahar
+        Card::define_purple_cards(3,"zemestan");//set zemestan
+        Card::define_purple_cards(3,"shir_dokht");//set shir_dokht
+        Card::define_purple_cards(6,"tabl_zan");//set shir_dokht
+        Card::shuffle_purple_cards();
+
+
     }
     ///getter
 
@@ -39,8 +56,24 @@ using namespace std;
     }
     ///method
 
-    void Card::shuffle_cards()
+    void Card::shuffle_purple_cards()
     {
+
+        int Shuff = rand() % 4 + 1;
+        for(int i = 0; i < 20 ;i++)
+        {
+            swap(purple_cards[i],purple_cards[Shuff]);
+        }
+
+    }
+    void Card::shuffle_yellow_cards()
+    {
+
+        int Shuff = rand() % 4 + 1;
+        for(int i = 0; i < 30 ;i++)
+        {
+            swap(yellow_cards[i],yellow_cards[Shuff]);
+        }
 
     }
 
@@ -55,6 +88,11 @@ using namespace std;
 
     void Card::define_yellow_cards(int index,int number)
     {
+
+        for(int i = 0; i < index ;i++)
+        {
+            yellow_cards.push_back(number);
+        }
 
     }
 
