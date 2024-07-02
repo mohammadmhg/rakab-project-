2ca973f10374ee51340d0f350c9b9fac1c32f3a2
mohammadmhg <mohammadmhg83@gmail.com>
2024-07-02 19:29:40 +0330

    added the body of war sign class

---
+++ War_sign.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "War_sign.h"
+
+using namespace std;
+
+    War_Sign::War_Sign(){}
+    War_Sign::War_Sign(string data): war(data){}
+    ///setter
+    void War_Sign::set_war_sign(string city)
+    {
+        this->war = city;
+    }
+    ///getter
+    string War_Sign::get_war_sign() const
+    {
+        return war;
+    }
+        ///method
+    void War_Sign::define_war_sing()
+    {
+        string temp_sign;
+        cin>> temp_sign;
+        while (temp_sign!="BELLA"&&temp_sign!="CALINE"&&temp_sign!="ENNA"&&temp_sign!="ATELA"&&temp_sign!="PLADACI"&&temp_sign!="BORGE"&&temp_sign!="DIMASE"&&temp_sign!="MORINA"&&
+        temp_sign!="OLIVADI"&&temp_sign!="ROLLO"&&temp_sign!="TALMONE"&&temp_sign!="ARMENTO"&&temp_sign!="LIA"&&temp_sign!="ELINIA")
+        {
+            cout <<"WRONG NAME!!"<<endl;
+            cin>>temp_sign;
+        }
+        War_Sign::set_war_sign(temp_sign);
+    }
