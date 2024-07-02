697c669965d6a03d4adfd623ba96a2c274ce7555
mohammadmhg <mohammadmhg83@gmail.com>
2024-07-02 20:02:47 +0330

    defined body of peace sign(it may change)

---
+++ Peace_sign.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include "Peace_sign.h"
+
+using namespace std;
+
+    ///constructor
+    Peace_Sign::Peace_Sign(){}
+    Peace_Sign::Peace_Sign(string name): peace(name){}
+    ///setter
+    void Peace_Sign::set_peace_sign(string name)
+    {
+        this->peace = name;
+    }
+    ///getter
+    string Peace_Sign::get_peace_sign()
+    {
+        return peace;
+    }
+    ///method
+    void Peace_Sign::define_peace_sign()
+    {
+        string temp_sign;
+        cin>> temp_sign;
+        while (temp_sign!="BELLA"&&temp_sign!="CALINE"&&temp_sign!="ENNA"&&temp_sign!="ATELA"&&temp_sign!="PLADACI"&&temp_sign!="BORGE"&&temp_sign!="DIMASE"&&temp_sign!="MORINA"&&
+        temp_sign!="OLIVADI"&&temp_sign!="ROLLO"&&temp_sign!="TALMONE"&&temp_sign!="ARMENTO"&&temp_sign!="LIA"&&temp_sign!="ELINIA")
+        {
+            cout <<"WRONG NAME!!"<<endl;
+            cin>>temp_sign;
+        }
+        Peace_Sign::set_peace_sign(temp_sign);
+    }
+
