435f9b026171b3450944f30d64008ba9f95ba08a
mohammadmhg <mohammadmhg83@gmail.com>
2024-07-05 14:06:45 +0330

    corrected the name of data

--- Control_card.h
+++ Control_card.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #ifndef CONTROL_CARD_H
 #define CONTROL_CARD_H
+
 using namespace std;
 
 
@@ -16,7 +17,7 @@ public:
 
     void set_winner(bool);
 
-    void set_most_powerful();
+    void set_most_powerful(int);
 
     void re_set_most_powerful();
     ///getter
@@ -41,11 +42,11 @@ public:
     void set_zemestan_ppwer();//set power when is zemestan
 
 private:
-    bool Winner = false;
-    int Power = 0;
-    static int Biggest_Played_Card;
-    static int Most_Powerful;
+    bool winner = false;
+    int power = 0;
+    static int biggest_played_card;
+    static int most_powerful;
 };
-int Control_Cards::Biggest_Played_Card = 0;
-int Control_Cards::Most_Powerful = 0;;
+int Control_Cards::biggest_played_card = 0;
+int Control_Cards::most_powerful = 0;;
 #endif
