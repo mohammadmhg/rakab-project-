4d6793aace1ff820d582d799fbc871ba5da5bd92
mohammadmhg <mohammadmhg83@gmail.com>
2024-07-05 13:39:38 +0330

    added the some methods

--- Control_card.h
+++ Control_card.h
@@ -1,5 +1,8 @@
+#include <iostream>
 #ifndef CONTROL_CARD_H
 #define CONTROL_CARD_H
+using namespace std;
+
 
 class Control_Cards{///this class this for controlling the power of each player army(include normal and some spacial cards related to Army power)
 public:
@@ -26,4 +29,23 @@ public:
 
     int get_biggest_card();
 
+    ///methods
+    void calculate_yellow_card_power();
+
+    void calculate_purple_card_power();//set power by purple cards
+
+    void set_bahar_power();
+
+    void set_biggest_card();//set the biggest played soldier card in game
+
+    void set_zemestan_ppwer();//set power when is zemestan
+
+private:
+    bool Winner = false;
+    int Power = 0;
+    static int Biggest_Played_Card;
+    static int Most_Powerful;
+};
+int Control_Cards::Biggest_Played_Card = 0;
+int Control_Cards::Most_Powerful = 0;;
 #endif
