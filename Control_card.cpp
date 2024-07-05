dbdcf40bbb5c349f8b7b7f203d85682b58e6dd81
mohammadmhg <mohammadmhg83@gmail.com>
2024-07-05 15:46:06 +0330

    added static data and some methoeds

--- Control_card.cpp
+++ Control_card.cpp
@@ -6,6 +6,10 @@
 
 
 
+int Control_Cards::biggest_played_card = 0;
+int Control_Cards::most_powerful = 0;
+
+
 
 using namespace std;
 
@@ -62,6 +66,53 @@ using namespace std;
     {
         return biggest_played_card;
     }
+
+    ///methods
+
+    void Control_Cards::calculate_yellow_card_power(const Gameplay game)
+    {//set power by yellow cards
+
+        for(int i = 0 ; i < game.get_play_cards() ;i++)
+        {
+
+            if (game.get_play_cards_data(i) != "matarsak"&&game.get_play_cards_data(i) != "shir_dokht"&&game.get_play_cards_data(i) != "tabl_zan"&&game.get_play_cards_data(i) != "bahar"&&
+               game.get_play_cards_data(i) !="zemestan"&&game.get_play_cards_data(i) != "Empty")
+            {
+
+                string temp_data = game.get_play_cards_data(i);
+                power =power + stoi(temp_data);
+
+            }
+        }
+    }
+
+
+    void Control_Cards::calculate_purple_card_power(const Gameplay game)
+    {//set power by purple cards
+        bool tabl_zan = false;
+        int temp_shir_dokht = 0;
+        for(int i = 0; i < game.get_play_cards() ;i++)
+            {
+
+            if(game.get_play_cards_data(i) == "tabl_zan" && tabl_zan == false)
+            {
+                tabl_zan = true;
+                power = power * 2;
+            }
+
+            if(game.get_play_cards_data(i) == "shir_dokht")
+            {
+                temp_shir_dokht = temp_shir_dokht + 10;
+            }
+
+        }
+        power = power + temp_shir_dokht;
+
+    }
+
+
+
+
 
 
 
