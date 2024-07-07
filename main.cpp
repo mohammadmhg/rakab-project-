6313d9b7ac3c065e64204e06371246566b84e524
mohammadmhg <mohammadmhg83@gmail.com>
2024-07-07 20:01:14 +0330

    define some methods(not finished)

--- main.cpp
+++ main.cpp
@@ -48,10 +48,132 @@ public:
             Gameplay b;
             game_gameplay.push_back(b);
         }
+
+        for(int i = 0; i< play.get_number_of_player() ; i++)
+        {
+            game_gameplay[i].setting_card(game_card,play.get_conquer_cities_number(i) );
+        }
+
+        game_started(play,game_city,game_gameplay);
+
     }
 
-    void game_started()
+    void game_started(Players play,vector <City> game_city,vector <Gameplay> game_gameplay)
     {
+        while(!end_of_game)
+        {
+            if( game_gameplay[0].get_empty_hand_players() >= (play.get_number_of_player() - 1) )
+            {
+                game_gameplay.clear();
+                for(int i = 0; i < play.get_number_of_player(); i++)
+                {
+                    Gameplay b;
+                    game_gameplay.push_back(b);
+                }
+                for(int i = 0; i< play.get_number_of_player() ; i++)
+                {
+                    game_gameplay[i].setting_card(game_card,play.get_conquer_cities_number(i) );
+                }
+            }
+            vector <Control_Cards> game_control( play.get_number_of_player() );
+            for(int i = 0; i < play.get_number_of_player(); i++)
+            {
+                Control_Cards b;
+                game_control.push_back(b);
+            }
+
+            for(int i = 0 ;i < play.get_number_of_player();i++)
+            {
+                game_gameplay[i].print_cards(i,play);///this function is only for print
+            }
+
+            int players_index = game_gameplay[0].get_first_attacker();///the first_attacker is index of first person who start the game so to continue in order we set Players_index and use it for++ in"for" only
+            system("cls");
+            warzone.define_war_sign();
+
+            for(int i = 0; i < play.get_number_of_player();i++)//to check that player can Enter a name of a EMPTY city
+            {
+                while(game_city[i].check_taken_cities( warzone.get_war_sign() ) )
+                {
+                        cout << "Invalid City!!" << endl;
+                        i = 0;
+                        warzone.define_war_sign();
+
+                }
+            }
+
+
+
+            for(int i =0; i< play.get_number_of_player() ;i++)
+            {
+                if( game_gameplay[i].get_bahar() )
+                {
+                    game_control[i].calculate_yellow_card_power( game_gameplay[i] );
+                    game_control[i].calculate_purple_card_power( game_gameplay[i] );
+                    game_control[i].set_bahar_power( game_gameplay[i] );
+                }
+                else if (game_gameplay[i].get_zemestan() )
+                {
+                    game_control[i].calculate_yellow_card_power( game_gameplay[i] );
+                    game_control[i].set_zemestan_power( game_gameplay[i] );
+                    game_control[i].calculate_purple_card_power( game_gameplay[i] );
+                }
+                else
+                {
+                    game_control[i].calculate_yellow_card_power( game_gameplay[i] );
+                    game_control[i].calculate_purple_card_power( game_gameplay[i] );
+                }
+            }
+
+            system("cls");
+            for(int i =0 ; i< play.get_number_of_player() ; i++)
+            {
+                game_gameplay[i].re_set_pass();
+                if (game_control[i].get_power() > game_control[i].get_most_powerful() )
+                {
+                    game_control[i].set_most_powerful( game_control[i].get_power() );
+                    game_gameplay[i].set_first_attacker(i);
+                    game_control[i].set_winner(true);
+
+                }
+            }
+            system("cls");
+            cout << "Army_power in order from Player1 to Player" <<play.get_number_of_player()<<endl;
+            for(int i =0 ; i < play.get_number_of_player();i++)
+            {
+                cout << game_control[i].get_power()<<"  ";
+            }
+            getch();
+            if ( game_control[game_gameplay[0].get_first_attacker()].get_winner() )
+            {
+                game_map.round_end( game_gameplay[0].get_first_attacker() );
+                play.set_conquer_cities_number( game_gameplay[0].get_first_attacker() );
+                game_city[game_gameplay[0].get_first_attacker()].set_city( warzone.get_war_sign() );
+            }
+
+
+            for(int k = 0; k < play.get_number_of_player() ;k++)
+            {
+                if (play.get_conquer_cities_number(k) >= 3)
+                {
+                    if(game_city[k].set_adjacent_cities() )
+                    {//end of game by adjacent
+                        game_map.winner_player(k);
+                        end_of_game = true;
+                    }
+                    if(play.get_conquer_cities_number(k) >= 5)
+                    {//end of game by taking the citys
+                        game_map.winner_player(k);
+                        end_of_game = true;
+                    }
+                }
+            }
+            winner = false;
+
+            game_control[0].re_set_most_powerful();
+            game_gameplay[0].set_seasons();
+            game_control[0].set_biggest_card_played();
+        }//end_of_game while
 
     }
 
