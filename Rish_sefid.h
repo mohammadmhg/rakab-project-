81555ca523eadfb028cee4d84ded4f6b364bd85e
mohammadmhg <mohammadmhg83@gmail.com>
2024-07-08 18:27:54 +0330

    changed data and method and added setter

--- Rish_sefid.h
+++ Rish_sefid.h
@@ -1,5 +1,5 @@
 #include <string>
-#include "City.h"
+#include <vector>
 #ifndef RISH_SEFID_H
 #define RISH_SEFID_H
 
@@ -11,22 +11,25 @@ public:
     ///constructor
     Rish_Sefid();
 
+    ///setter
+    void set_biggest_card_value(int);
+    void set_number_of_players(int);
+
+
     ///getter
     int get_last_played_card_index();
 
     bool get_rish_sefid_card();
 
     ///method
-    void setting_rish_sefid(int);
-
-    void setting_peace_sign(vector <City>);
-
-
+    void setting_rish_sefid(int,vector <string>&);
 
 
 private:
     static int last_played_card_index;
     static bool rish_sefid_card;
+    static int number_of_players;
+    static int biggest_card_value;
 
 };
 
