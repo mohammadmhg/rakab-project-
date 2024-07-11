ca3c4566f5f19c92f0593c0778215b9c39d10407
mohammadmhg <mohammadmhg83@gmail.com>
2024-07-11 13:44:30 +0330

    added new data and method to header

--- Saved_game.h
+++ Saved_game.h
@@ -20,17 +20,19 @@ public:
 
     void saving_gamplay_cards(const int,const vector<Gameplay>&);
 
-    void saving_gamplay_data(const int,const vector<Gameplay>&);
+    void saving_gamplay_data(const int,const vector<Gameplay>&,const Rish_Sefid&);
 
+    void saving_control_data(const int,const Control_Cards&);
 
-
-
+    void saving_cities_data(const int,const War_Sign&,const Peace_Sign&,const vector<City>&);
 
 private:
     ofstream output_player_identity;
     ofstream output_gameplay_cards;
     ofstream output_gameplay_play_card;
     ofstream output_gameplay_data;
+    ofstream output_control_data;
+    ofstream output_cities_data;
 };
 
 
