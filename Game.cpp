e2c91a7cd5f07cc8bcf8247495fe10ea08ddaedd
mohammadmhg <mohammadmhg83@gmail.com>
2024-07-12 20:05:01 +0330

    declared new method on body of class

--- Game.cpp
+++ Game.cpp
@@ -43,3 +43,53 @@ using namespace std;
         }
 
     }
+
+    void Game::setting_game()
+    {
+        play.set_player_correct();
+
+        for(int i = 0 ; i< play.get_number_of_player();i++)
+        {
+            City x;
+            game_city.push_back(x);
+        }
+
+        for(int i = 0; i < play.get_number_of_player(); i++)
+        {
+            Gameplay b;
+            game_gameplay.push_back(b);
+        }
+
+        game_control.setting_size(play.get_number_of_player());
+
+        for(int i = 0; i< play.get_number_of_player() ; i++)
+        {
+            game_gameplay[i].setting_card(game_card,play.get_conquer_cities_number(i) );
+        }
+
+        game_started();
+
+    }
+
+    void Game::continue_the_last_game()
+    {
+        system("cls");
+        game_saver.uploading_the_player_identity(play);
+        for(int i = 0; i < play.get_number_of_player() ;i++)
+        {
+            Gameplay x;
+            game_gameplay.push_back(x);
+        }
+        game_saver.uploading_gamplay_cards(play.get_number_of_player(),game_gameplay);
+        game_saver.uploading_gamplay_data(play.get_number_of_player(),game_gameplay,rish_sefid_handel);
+        game_control.setting_size(play.get_number_of_player());
+        game_saver.uploading_control_data(play.get_number_of_player(),game_control);
+        for(int i = 0 ; i< play.get_number_of_player();i++)
+        {
+            City z;
+            game_city.push_back(z);
+        }
+        game_saver.uploading_cities_data(play.get_number_of_player(),warzone,peacezone,game_city);
+        int players_index = game_gameplay[0].get_players_turn();///the first_attacker is index of first person who start the game so to continue in order we set Players_index and use it for++ in"for" only
+        starting_the_round(players_index);
+    }
