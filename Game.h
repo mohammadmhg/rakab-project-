43643e3dbab473cf46bc9cb1ba99b0adbacee617
mohammadmhg <mohammadmhg83@gmail.com>
2024-07-12 19:56:08 +0330

    declared the deader of main class(not finished)

---
+++ Game.h
@@ -0,0 +1,50 @@
+#include "Menu.h"
+#include "Rish_sefid.h"
+#include "Peace_sign.h"
+#include "Player.h"
+#include "Gameplay.h"
+#include "Print_game.h"
+#include "War_sign.h"
+#include "Control_card.h"
+#include "City.h"
+#include "Saved_game.h"
+#ifndef GAME_H
+#define GAME_H
+
+class Game{
+public:
+    Game();
+
+    void starting_the_round(int);
+
+    void set_power_of_army();
+
+    void set_most_powerful_army();
+
+    void show_power_of_army();
+
+    void set_winner_of_battle();
+
+    void validation_of_peace_sign();
+
+    void saving_the_game_data();
+
+private:
+    Menus user;
+    War_Sign warzone;
+    Card game_card;
+    Print_Game game_map;
+    Peace_Sign peacezone;
+    Rish_Sefid rish_sefid_handel;
+    Saved_Game game_saver;
+    Players play;
+    vector <Gameplay> game_gameplay;
+    Control_Cards game_control;
+    vector <City> game_city;
+    bool winner = false;
+    bool end_of_game = false;
+    bool exit = false;
+
+};
+
+#endif
