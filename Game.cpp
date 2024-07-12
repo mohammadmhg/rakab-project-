5f12722fae1f3f9084af17c41b4d5fc142d842bc
mohammadmhg <mohammadmhg83@gmail.com>
2024-07-12 20:07:17 +0330

    defined new methods on main class

--- Game.cpp
+++ Game.cpp
@@ -93,3 +93,107 @@ using namespace std;
         int players_index = game_gameplay[0].get_players_turn();///the first_attacker is index of first person who start the game so to continue in order we set Players_index and use it for++ in"for" only
         starting_the_round(players_index);
     }
+
+    void Game::game_started()
+    {
+        while(!end_of_game)
+        {
+            if( game_gameplay[0].get_empty_hand_players() >= (play.get_number_of_player() - 1) )
+            {
+                game_gameplay[0].re_set_empty_hand_players();
+                game_gameplay.clear();
+                for(int i = 0; i < play.get_number_of_player(); i++)
+                {
+                    Gameplay b;
+                    game_gameplay.push_back(b);
+                }
+                for(int i = 0; i< play.get_number_of_player() ; i++)
+                {
+                    game_gameplay[i].setting_card(game_card,play.get_conquer_cities_number(i) );
+                }
+            }
+
+            for(int i = 0 ;i < play.get_number_of_player();i++)
+            {
+                game_gameplay[i].print_cards(i,play);///this function is only for print
+            }
+
+            if( rish_sefid_handel.get_used_rish_sefid_card() )
+            {
+                rish_sefid_handel.set_used_rish_sefid_card(false);
+                peacezone.define_peace_sign(rish_sefid_handel.get_last_played_card_index() );
+                validation_of_peace_sign();
+            }
+
+            system("cls");
+            warzone.define_war_sign( game_control.get_battle_city_chooser() );
+            for(int i = 0; i < play.get_number_of_player();i++)//to check that player can Enter a name of a EMPTY city
+            {
+                while(game_city[i].check_taken_cities( warzone.get_war_sign(),peacezone.get_peace_sign() ) )
+                {
+                        cout << "Invalid City!!" << endl;
+                        i = 0;
+                        warzone.define_war_sign( game_control.get_battle_city_chooser() );
+                }
+            }
+            int players_index = game_control.get_first_attacker();///the first_attacker is index of first person who start the game so to continue in order we set Players_index and use it for++ in"for" only
+            starting_the_round(players_index);
+            set_power_of_army();
+            set_most_powerful_army();
+            show_power_of_army();
+            set_winner_of_battle();
+
+            winner = false;
+            game_control.re_set_most_powerful( play.get_number_of_player() );
+            game_gameplay[0].set_seasons();
+            game_control.set_biggest_card_played(0);
+        }//end_of_game while
+    }
+
+    void Game::starting_the_round(int players_index)
+    {
+        while(!winner)
+        {
+            for(players_index ; players_index < play.get_number_of_player(); players_index++)
+            {
+                saving_the_game_data();
+                game_map.showing_map( play.get_number_of_player(),warzone.get_war_sign(),game_gameplay,game_city);
+                game_gameplay[players_index].show_saturation(players_index,play.get_number_of_player(),game_control.get_biggest_card() );
+                game_gameplay[players_index].check_empty_cards();
+
+                if(game_gameplay[players_index].get_pass() == true && game_gameplay[players_index].get_pass_counted() == false)
+                {
+                    game_gameplay[players_index].set_pass_counted(true);//to sure that we already count this pass
+                    game_gameplay[players_index].set_passed_players();
+                    game_control.set_handel_passed_players(players_index);
+                }
+                if (rish_sefid_handel.get_rish_sefid_card() )
+                {
+                    game_gameplay[players_index].handel_rish_sefid(play.get_number_of_player(),game_gameplay);
+                    game_control.set_biggest_card_played(0);
+                    rish_sefid_handel.set_rish_sefid_card(false);
+                }
+
+                game_control.set_biggest_card(game_gameplay[players_index]);//set the biggest played card
+
+                if( game_gameplay[players_index].get_help() )
+                {
+                    game_gameplay[players_index].set_help(false);
+                    players_index--;
+                }
+                if( game_gameplay[players_index].get_parcham_dar() )
+                {
+                    winner = true;//ending while
+                    game_gameplay[players_index].re_set_parcham_dar();
+                    break;
+                }
+            }
+            players_index = 0;//to restart
+            if (game_gameplay[players_index].get_passed_players() >= play.get_number_of_player() )
+            {//to end the round on fight in a city
+                game_gameplay[players_index].re_set_passed_players();
+                winner = true;
+            }
+        }//winner while
+
+    }
