eb4b7064d25ccf59b28b38139e09ba4997af8c13
mohammadmhg <mohammadmhg83@gmail.com>
2024-07-16 14:09:54 +0330

    changed the validations and add new method

--- Gameplay.cpp
+++ Gameplay.cpp
@@ -282,7 +282,8 @@ int Gameplay::players_turn = 0;
         cout << endl<<endl<<"Please Enter the value of The Card That you want to play(Enter 0 or say pass if You want to Pass): " <<endl;
         string input;
         getline(cin,input);
-        while(input!="help"&&input!="pass"&&input!="help bahar"&&input!="help matarsak"&&input!="help shir_dokht"&&input!="help zemestan"&&input!="help tabl_zan"&&input!="0")
+        while(input!="help"&&input!="pass"&&input!="help bahar"&&input!="help matarsak"&&input!="help shir_dokht"&&input!="help zemestan"&&input!="help tabl_zan"&&input!="0"
+              &&input!="help shir_zan"&&input!="help rish_sefid"&&input!="help parcham_dar")
         {
             if( Gameplay::check_exist_card(input) )
             {
@@ -316,13 +317,13 @@ int Gameplay::players_turn = 0;
             menu.menu_of_opsions();
             return;
         }
-        if(input=="help bahar"||input=="help matarsak"||input=="help shir_dokht"||input=="help zemestan"||input=="help tabl_zan")
+        if(input=="help bahar"||input=="help matarsak"||input=="help shir_dokht"||input=="help zemestan"||input=="help tabl_zan"||input=="help parcham_dar"||
+           input=="help shir_zan"||input=="help rish_sefid")
         {
             set_help(true);
             cards_helping_menus(input);
             return;
         }
-
         Gameplay::input_chosen_card(input);
         Gameplay::setting_the_input_card();
     }
@@ -345,16 +346,11 @@ int Gameplay::players_turn = 0;
         cout<<endl << "Please Enter the Value of The soldier Card you Want to Put Back:(only inter Value of card if there is no soldier card Enter 0)"<<endl;
         string input;
         cin>> input;
-        while(input != "0"&&input != "1"&&input !="2"&&input != "3"&&input != "4"&&input != "5"&&input != "6"&&input != "7"&&input != "8"&&input != "9"&&input != "10")
-        {
-            cout <<"ONLY SOLDIERS PLEASE!!"<<endl;
-            cin>> input;
-        }
         if(input == "0")
         {
             return;
         }
-        while( check_exist_card(input)== false )
+        while( check_exist_played_card(input)== false )
         {
             cout << "There is no Card with this Value!!"<<endl;
             cin>> input;
@@ -377,6 +373,23 @@ int Gameplay::players_turn = 0;
                 break;
             }
         }
+    }
+
+    bool Gameplay::check_exist_played_card(string input)const
+    {//validation of matarsak
+        if(input != "0"&&input != "1"&&input !="2"&&input != "3"&&input != "4"&&input != "5"&&input != "6"&&input != "10")
+        {
+            return false;
+        }
+
+        for(int i = 0 ; i < play_cards.size() ; i++)
+        {
+            if(input == play_cards[i] && input != "Empty")
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     void Gameplay::define_players_turn(int number,int number_of_players)
@@ -412,17 +425,19 @@ int Gameplay::players_turn = 0;
         }
         if(temp == "help rish_sefid")
         {
-
+            menu.rish_sefid_help();
+            return;
         }
         if(temp == "help shir_zan")
         {
-
+            menu.shir_zan_help();
+            return;
         }
         if(temp == "help parcham_dar")
         {
-
+            menu.parcham_dar_help();
+            return;
         }
-
     }
 
     void Gameplay::input_chosen_card(string temp)
@@ -470,8 +485,7 @@ int Gameplay::players_turn = 0;
             for (int i = 0; i < game_gameplay[j].play_cards.size() ;i++)
             {
                 if(game_gameplay[j].play_cards[i] == "1"||game_gameplay[j].play_cards[i] == "2"||game_gameplay[j].play_cards[i] == "3"||game_gameplay[j].play_cards[i] == "4"||
-                   game_gameplay[j].play_cards[i] == "5"||game_gameplay[j].play_cards[i] == "6"||game_gameplay[j].play_cards[i] == "7"||game_gameplay[j].play_cards[i] == "8"||
-                   game_gameplay[j].play_cards[i] == "9"||game_gameplay[j].play_cards[i] == "10" )
+                   game_gameplay[j].play_cards[i] == "5"||game_gameplay[j].play_cards[i] == "6"||game_gameplay[j].play_cards[i] == "10" )
                 {
                     if( stoi(game_gameplay[j].play_cards[i]) == rish_sefid_handel.get_biggest_card_value() )
                     {
