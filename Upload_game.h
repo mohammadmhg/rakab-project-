afc3b298b7161b286f2c32db82b9f7f01168abc0
mohammadmhg <mohammadmhg83@gmail.com>
2024-08-08 21:29:05 +0330

    changed the declaration after added extra part and new class in header

--- Upload_game.h
+++ Upload_game.h
@@ -6,6 +6,7 @@
 #include "War_sign.h"
 #include "Control_card.h"
 #include "City.h"
+#include "Handle_data.h"
 #include <fstream>
 
 #ifndef UPLOAD_GAME_H
@@ -19,23 +20,21 @@ public:
     Upload_Game();
 
     void uploading_the_player_identity(Players&);
-
     void uploading_gamplay_cards(const int,vector<Gameplay>&);
-
-    void uploading_gamplay_data(const int,vector<Gameplay>&,Rish_Sefid&);
-
+    void uploading_gamplay_data(const int,vector<Gameplay>&,Rish_Sefid&,Handle_Data&);
     void uploading_control_data(const int,Control_Cards&);
-
     void uploading_cities_data(const int,War_Sign &,Peace_Sign &,vector <City> &);
+    void uploading_red_cards_data(const int,vector <City>&);
 
 private:
+
     ifstream input_player_identity;
     ifstream input_gameplay_cards;
     ifstream input_gameplay_play_card;
     ifstream input_gameplay_data;
     ifstream input_control_data;
     ifstream input_cities_data;
-
+    ifstream input_red_cards_data;
 };
 
 
