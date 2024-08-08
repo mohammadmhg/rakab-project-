67292d0103a1d54b658a183f985a908f2277e1e0
mohammadmhg <mohammadmhg83@gmail.com>
2024-08-08 21:35:54 +0330

    added the changes into header of game class and added new data

--- Game.h
+++ Game.h
@@ -8,8 +8,11 @@
 #include "Control_card.h"
 #include "City.h"
 #include "Saved_game.h"
+#include "Upload_game.h"
+#include "Handle_data.h"
 #ifndef GAME_H
 #define GAME_H
+using namespace std;
 
 class Game{
 public:
@@ -18,29 +21,22 @@ public:
     bool get_exit_game()const;
 
 ///method
+
     void open_the_menu();
-
     void setting_game();
-
     void continue_the_last_game();
-
     void game_started();
-
     void starting_the_round(int);
-
     void set_power_of_army();
-
     void set_most_powerful_army();
-
     void show_power_of_army();
-
     void set_winner_of_battle();
-
     void validation_of_peace_sign();
-
     void saving_the_game_data();
+    void control_conditions(int);
 
 private:
+
     Menus user;
     War_Sign warzone;
     Card game_card;
@@ -49,6 +45,7 @@ private:
     Rish_Sefid rish_sefid_handel;
     Saved_Game game_saver;
     Players play;
+    Handle_Data game_card_data;
     vector <Gameplay> game_gameplay;
     Control_Cards game_control;
     vector <City> game_city;
