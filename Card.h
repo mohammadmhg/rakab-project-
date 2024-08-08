8e044f930a79baf519fc0fd2f78cda0b79bc7207
mohammadmhg <mohammadmhg83@gmail.com>
2024-08-08 21:24:46 +0330

    defined the ne methods and new data into the header

--- Card.h
+++ Card.h
@@ -7,25 +7,31 @@ using namespace std;
 class Card{
 public:
     Card();
-    Card(string,int);
+    Card(string,string);
     ///setter
 
     void set_yellow_card();
     void set_purple_card();
+    void set_red_card();
     ///getter
 
-    int get_yellow_card(int) const;
+    string get_yellow_card(int) const;
     string get_purple_card(int) const;
+    string get_red_card(int) const;
     ///method
 
     void shuffle_yellow_cards();
     void shuffle_purple_cards();
+    void shuffle_red_cards();
 
 private:
+
     vector <string> purple_cards;
-    vector <int> yellow_cards;
+    vector <string> yellow_cards;
+    vector <string> red_cards;
     void define_purple_cards(int,string);
-    void define_yellow_cards(int,int);
+    void define_yellow_cards(int,string);
+    void define_red_cards(int,string);
 
 };
 
