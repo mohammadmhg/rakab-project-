da47ba2c6f6dfb189002c58c6f695a965f5aedfe
mohammadmhg <mohammadmhg83@gmail.com>
2024-08-08 21:27:25 +0330

    defined the new red cities into class (extra part)

--- City.h
+++ City.h
@@ -6,6 +6,30 @@
 #define CITY_H
 using namespace std;
 
+struct Red_color_city{
+
+    vector <vector<string>> red_cities = {
+    {"BELLA","ghale","kohestan"},
+    {"LIA","ghale","jangal"},
+    {"ATELA","jangal","kohestan"},
+    {"CALINE"},
+    {"BORGE","ghale"},
+    {"PLADACI"},
+    {"DIMASE","kohestan"},
+    {"MORINA","kohestan","ghale","jangal"},
+    {"OLIVADI","ghale"},
+    {"ROLLO","ghale"},
+    {"TALMONE","jangal"},
+    {"ARMENTO","jangal","ghale"},
+    {"ENNA"},
+    {"ELINIA","jangal","kohestan"},
+    {"ATEN",}
+
+    };
+
+    vector <int> taken_cities_number = {7,6,5,0,4,0,3,9,4,4,2,6,0,5,0};
+};
+
 class City{//to control the Map of game and the citys(include every methods and datas needed for controlling the citys)
 public:
     City();
@@ -13,24 +37,29 @@ public:
     ///setter
 
     void set_city(string);
-
+    void set_red_cities(int,int,string);
+    void set_taken_cities_number(int,int);
     ///getter
 
     string get_city(int)const;
-
     int get_size_of_city()const;
-
+    string get_red_cities(int,int)const;
+    int get_taken_cities_number(int)const;
+    int get_red_cities_size(int)const;
     ///method
+
     void show_taken_cities() const;
-
     bool check_taken_cities(string,string);//check that player do not enter a name of a taken city
-
     bool set_adjacent_cities();//check adjacent for the taken citys by a player
-
+    void setting_herkol_effect(string);
+    void setting_kooh_shekan_effect(string);
+    void setting_rooh_jangal_effect(string);
+    bool open_city(string);
 
 private:
-    vector <string>taken_city;
 
+    Red_color_city spacial_cities;
+    vector <string>taken_city;
     void check_second_adjacent(int &,int,unordered_map<string, vector<string>>,string,int);
 
 };
